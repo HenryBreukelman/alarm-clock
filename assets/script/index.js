b8d460f005af4e1b97751cd81ee56d85e2e861fe
Henry <hbreukelman20@gmail.com>
2024-03-18 14:20:29 -0500

    seting time

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,10 +1,22 @@
 'use strict';
 
+//query selectors
+const time = document.querySelector('.time');
 const hour = document.querySelector('.hour');
 const minute = document.querySelector('.minute');
 const newAlarm = document.querySelector('.set-alarm');
 
+const timeNow = new Date();
+
+//setting time
+const setTime = () => {
+  time.innerText = timeNow.toString().substring(16, 21)
+}
+
+//event listeners
 newAlarm.addEventListener('click', () => {
   console.log(newAlarm)
 });
 
+window.addEventListener('load', setTime);
+setInterval(setTime, 10000);
