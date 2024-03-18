02d7bd273b2ce5a97a553420506b0439bf1cc450
Henry <hbreukelman20@gmail.com>
2024-03-18 15:42:47 -0500

    worked on js

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,21 +2,42 @@
 
 //query selectors
 const time = document.querySelector('.time');
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minute');
+const alarm = document.querySelector('.alarm')
 const newAlarm = document.querySelector('.set-alarm');
 
-const timeNow = new Date();
+let alarmTime = '15:08';
+
 
 //setting time
-const setTime = () => {
-  time.innerText = timeNow.toString().substring(16, 21)
+function setTime() {
+  const timeNow = new Date();
+  time.innerText = timeNow.toString().substring(16, 21);
+  trigerAlarm;
+}
+
+//set alarm
+function setAlarm() {
+  const hour = document.querySelector(('.hour').value);
+  const minute = document.querySelector(('.minute').value);
+  if (true) {
+    alarmTime = `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
+    console.log(hour);
+    console.log(minute);
+    alarm.innerText = alarmTime;
+  } else {
+    alert('Invalid time! Please enter valid hour (0-23) and minute (0-59).');
+  }
+}
+
+//triger alarm
+function trigerAlarm() {
+  if (time.innerText === alarmTime) {
+    console.log(hi);
+  }
 }
 
 //event listeners
-newAlarm.addEventListener('click', () => {
-  console.log(newAlarm)
-});
+newAlarm.addEventListener('click', setAlarm);
 
 window.addEventListener('load', setTime);
-setInterval(setTime, 10000);
+setInterval(setTime, 2000);
