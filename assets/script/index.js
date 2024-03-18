942347b58a5c8e5b626cc35d9f393a4751becfa3
Henry <hbreukelman20@gmail.com>
2024-03-18 13:30:11 -0500

    add basic code

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const newAlarm = document.querySelector('.set-alarm');
+
+newAlarm.addEventListener('click', () => {
+  console.log(newAlarm)
+});
+
